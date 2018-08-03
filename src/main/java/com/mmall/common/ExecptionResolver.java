a269e945d84644ba7e3cc76d28f249ee4fa20061
jie lu <461907111@qq.com>
2018-08-03 18:15:18 +0800

    add execption resolver to make sure client can't see the complete error

--- src/main/java/com/mmall/common/ExecptionResolver.java
+++ src/main/java/com/mmall/common/ExecptionResolver.java
@@ -1,4 +1,28 @@
 package com.mmall.common;
 
-public class ExecptionResolver {
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.HandlerExceptionResolver;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.view.json.MappingJacksonJsonView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@Component
+public class ExecptionResolver implements HandlerExceptionResolver {
+
+    @Override
+    public ModelAndView resolveException(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse, Object o, Exception e) {
+        log.error("{} Exception ", httpServletRequest.getRequestURI(), e);
+        //jackson 2.x 使用 MappingJackson2JsonView,目前使用1.9的版本
+        ModelAndView modelAndView = new ModelAndView(new MappingJacksonJsonView());
+
+        modelAndView.addObject("status", ResponseCode.ERROR.getCode());
+        modelAndView.addObject("msg", "详情请查看服务器异常日志");
+        modelAndView.addObject("data", e.toString());
+        return modelAndView;
+
+    }
 }
