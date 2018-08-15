958afa9db3d061913002f51a424d9844f396d2df
jie lu <461907111@qq.com>
2018-08-15 22:54:10 +0800

    rewrit the close order task ,make sure could work in redis env

--- src/main/java/com/mmall/task/CloseOrderTask.java
+++ src/main/java/com/mmall/task/CloseOrderTask.java
@@ -1,7 +1,9 @@
 package com.mmall.task;
 
+import com.mmall.common.Const;
 import com.mmall.service.IOrderService;
 import com.mmall.util.PropertiesUtil;
+import com.mmall.util.RedisShardedPoolUtil;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -15,9 +17,42 @@ public class CloseOrderTask {
     @Autowired
     private IOrderService iOrderService;
 
+//    @Scheduled(cron = "0 */1 * * * ?")//每分钟
+//    public void closeOrderTaskV1(){//在集群的情况下会出现异常，需要加锁进行协调处理
+//        int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2"));
+//        iOrderService.closeOrder(hour);
+//    }
+
     @Scheduled(cron = "0 */1 * * * ?")//每分钟
-    public void closeOrderTaskV1(){
+    public void closeOrderTaskV2(){
+        log.info("关闭订单定时任务启动");
+        long lockTimeout = Long.parseLong(PropertiesUtil.getProperty("lock.timeout","2"));
+        Long setnxResult = RedisShardedPoolUtil.setnx(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,String.valueOf(System.currentTimeMillis()+lockTimeout));
+
+        if (setnxResult != null && setnxResult.intValue() == 1){
+            //获取成功
+            //        iOrderService.closeOrder(hour);
+            closeOrder(Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        }else {
+            log.info("没有获取到分布式锁:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK);
+        }
+
+
+        log.info("关闭订单定时任务关闭");
+
+    }
+
+    private void closeOrder(String lockName){
+        RedisShardedPoolUtil.expire(lockName,50);//有效期5S防止死锁
+        log.info("获取{},ThreadName:{}",Const.REDIS_LOCK.CLOSE_ORDER_TASK_LOCK,Thread.currentThread().getName());
         int hour = Integer.parseInt(PropertiesUtil.getProperty("close.order.task.time.hour","2"));
         iOrderService.closeOrder(hour);
+        RedisShardedPoolUtil.del(lockName);//释放分布式锁
+
     }
+
+
+
+
+
 }
